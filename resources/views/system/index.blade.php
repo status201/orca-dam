9c5a65661871825ed22abc6806fc2c796d224af8
status201 <gijs.oliemans@gmail.com>
2026-02-06 11:08:19 +0100

    composer update, css tweaks

--- resources/views/system/index.blade.php
+++ resources/views/system/index.blade.php
@@ -11,7 +11,7 @@
     </div>
 
     <!-- Tab Navigation -->
-    <div class="mb-6 border-b border-gray-200 w-full overflow-x-auto">
+    <div class="tab-navigation-container mb-6 border-b border-gray-200 w-full overflow-y-hidden overflow-x-auto">
         <nav class="-mb-px flex space-x-8">
             <button @click="activeTab = 'overview'"
                     :class="activeTab === 'overview' ? 'border-orca-black text-orca-black' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'"
