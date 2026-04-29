a7a6cd57560e4085788377e8f2ffcc36b7c7047d
status201 <gijs.oliemans@gmail.com>
2026-04-29 15:22:16 +0200

    [FEATURE] TikZ Server Tool: animated GIF Upload destination folder

--- resources/views/tools/tikz-server.blade.php
+++ resources/views/tools/tikz-server.blade.php
@@ -590,7 +590,7 @@
                                 <select
                                     x-model="uploadFolder"
                                     title="{{ __('Destination folder') }}"
-                                    class="appearance-none pl-7 pr-7 py-1.5 font-mono text-xs border border-gray-200 rounded bg-white hover:border-orca-teal focus:outline-none focus:ring-1 focus:ring-orca-teal focus:border-orca-teal cursor-pointer max-w-[200px] truncate">
+                                    class="appearance-none bg-none pl-7 pr-7 py-1.5 font-mono text-xs border border-gray-200 rounded bg-white hover:border-orca-teal focus:outline-none focus:ring-1 focus:ring-orca-teal focus:border-orca-teal cursor-pointer max-w-[200px] truncate">
                                     <x-folder-tree-options :folders="$folders" :root-folder="$rootFolder" />
                                 </select>
                                 <i class="fas fa-chevron-down absolute right-2 top-1/2 -translate-y-1/2 text-gray-400 text-xs pointer-events-none"></i>
